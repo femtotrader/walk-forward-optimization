0be0d8ccbf3cf266512344668c1f3ff956702431
femtotrader <femto.trader@gmail.com>
2023-07-28 20:40:55 +0200

    disable wfo

--- tests/test_walk_forward_optimization.py
+++ tests/test_walk_forward_optimization.py
@@ -1,10 +1,13 @@
 from pytest import approx
 from walk_forward_optimization import WalkForwardOptimization
+import pandas as pd
 
 
 def test_walk_forward_optimization():
+    wfo_start = "2013-01-01"
+    wfo_end = "2023-01-01"
     wfo = WalkForwardOptimization(
-        start="2013-01-01", end="2023-01-01", n=7, ins_pct=0.75, anchored=False
+        start=wfo_start, end=wfo_end, n=7, ins_pct=0.75, anchored=False
     )
     print(f"ins: {wfo.ins}")
     print(f"oos: {wfo.oos}")
@@ -18,3 +21,45 @@ def test_walk_forward_optimization():
         print(f"\t{sample}: ", start, end, end - start + wfo.index.freq)
     assert stage == 7
     assert wfo.stages_count == 7
+
+
+def test_walk_forward_optimization_disabled_ins_only():
+    wfo_start = "2013-01-01"
+    wfo_end = "2023-01-01"
+    wfo = WalkForwardOptimization(
+        start=wfo_start, end=wfo_end, n=1, ins_pct=1.0, anchored=False
+    )
+    print(f"ins: {wfo.ins}")
+    print(f"oos: {wfo.oos}")
+    print(f"ins%: {wfo.ins_ratio}")  # 0.8 means 80% in sample / 20% out of sample
+    print(f"oos%: {wfo.oos_ratio}")
+    assert wfo.ins_ratio == approx(1.0, 0.01)
+    assert wfo.oos_ratio == approx(0.0, 0.01)
+    for i, (stage, sample, start, end) in enumerate(wfo.iterate(), start=1):
+        if i == 1:
+            assert stage == 1
+            assert sample == "ins"
+            assert start == pd.to_datetime(wfo_start)
+            assert end == pd.to_datetime(wfo_end)
+    assert wfo.stages_count == 1
+
+
+def test_walk_forward_optimization_disabled_oos_only():
+    wfo_start = "2013-01-01"
+    wfo_end = "2023-01-01"
+    wfo = WalkForwardOptimization(
+        start=wfo_start, end=wfo_end, n=1, ins_pct=0.0, anchored=False
+    )
+    print(f"ins: {wfo.ins}")
+    print(f"oos: {wfo.oos}")
+    print(f"ins%: {wfo.ins_ratio}")  # 0.8 means 80% in sample / 20% out of sample
+    print(f"oos%: {wfo.oos_ratio}")
+    assert wfo.ins_ratio == approx(0.0, 0.01)
+    assert wfo.oos_ratio == approx(1.0, 0.01)
+    for i, (stage, sample, start, end) in enumerate(wfo.iterate(), start=1):
+        if i == 1:
+            assert stage == 1
+            assert sample == "oos"
+            assert start == pd.to_datetime(wfo_start)
+            assert end == pd.to_datetime(wfo_end)
+    assert wfo.stages_count == 1
--- walk_forward_optimization/walk_forward_optimization.py
+++ walk_forward_optimization/walk_forward_optimization.py
@@ -6,6 +6,7 @@ class WalkForwardOptimization:
     def __init__(self, start, end, freq="D", n=7, ins_pct=0.75, anchored=False):
         index = pd.date_range(start, end, freq=freq)
         self.index = index
+        self._n = n
         n_index = len(self.index)
         ## Define coefficients of equations
         a = np.array([[1 - ins_pct, -ins_pct], [1, n]])
@@ -28,6 +29,7 @@ class WalkForwardOptimization:
 
     @property
     def stages_count(self):
+        stage = 0
         for stage, *_ in self.iterate():
             pass
         return stage
@@ -36,71 +38,74 @@ class WalkForwardOptimization:
         i = 0
         n_index = len(self.index)
         stage = 1
-        while True:
-            if i + self.ins < n_index:
-                if i + self.ins + self.oos < n_index:
-                    # if i + 2 * self.ins + self.oos < len(index):
-                    if self.anchored:
-                        yield (
-                            stage,
-                            "ins",
-                            self.index[0],
-                            self.index[i + self.ins - 1],
-                        )
-                        yield (
-                            stage,
-                            "oos",
-                            self.index[i + self.ins],
-                            self.index[i + self.ins + self.oos - 1],
-                        )
-                    else:
-                        yield (
-                            stage,
-                            "ins",
-                            self.index[i],
-                            self.index[i + self.ins - 1],
-                        )
-                        yield (
-                            stage,
-                            "oos",
-                            self.index[i + self.ins],
-                            self.index[i + self.ins + self.oos - 1],
-                        )
-                    stage += 1
-
-                    # else:
-                    #    yield (index[i], index[i + self.ins - 1]), (index[i + self.ins], index[-1])
-                else:
-                    if n_index - (i + self.ins) > self.oos / 2:
+        if self.ins_ratio == 1 and self._n <= 1:
+            yield 1, "ins", self.index[0], self.index[-1]
+        else:
+            while True:
+                if i + self.ins < n_index:
+                    if i + self.ins + self.oos < n_index:
+                        # if i + 2 * self.ins + self.oos < len(index):
                         if self.anchored:
-                            if self.ins > 0:
-                                yield (
-                                    stage,
-                                    "ins",
-                                    self.index[0],
-                                    self.index[i + self.ins - 1],
-                                )
+                            yield (
+                                stage,
+                                "ins",
+                                self.index[0],
+                                self.index[i + self.ins - 1],
+                            )
                             yield (
                                 stage,
                                 "oos",
                                 self.index[i + self.ins],
-                                self.index[-1],
+                                self.index[i + self.ins + self.oos - 1],
                             )
                         else:
-                            if self.ins > 0:
-                                yield (
-                                    stage,
-                                    "ins",
-                                    self.index[i],
-                                    self.index[i + self.ins - 1],
-                                )
+                            yield (
+                                stage,
+                                "ins",
+                                self.index[i],
+                                self.index[i + self.ins - 1],
+                            )
                             yield (
                                 stage,
                                 "oos",
                                 self.index[i + self.ins],
-                                self.index[-1],
+                                self.index[i + self.ins + self.oos - 1],
                             )
-                    stage += 1
-                i += self.oos
-            else:
-                break
+                        stage += 1
+
+                        # else:
+                        #    yield (index[i], index[i + self.ins - 1]), (index[i + self.ins], index[-1])
+                    else:
+                        if n_index - (i + self.ins) > self.oos / 2:
+                            if self.anchored:
+                                if self.ins > 0:
+                                    yield (
+                                        stage,
+                                        "ins",
+                                        self.index[0],
+                                        self.index[i + self.ins - 1],
+                                    )
+                                yield (
+                                    stage,
+                                    "oos",
+                                    self.index[i + self.ins],
+                                    self.index[-1],
+                                )
+                            else:
+                                if self.ins > 0:
+                                    yield (
+                                        stage,
+                                        "ins",
+                                        self.index[i],
+                                        self.index[i + self.ins - 1],
+                                    )
+                                yield (
+                                    stage,
+                                    "oos",
+                                    self.index[i + self.ins],
+                                    self.index[-1],
+                                )
+                        stage += 1
+                    i += self.oos
+                else:
+                    break
